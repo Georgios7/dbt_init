a9da3fc8673dbe1e6370e68f790a0ef280d9b6b6
gchatz7 <gchatzinis@gmail.com>
2023-06-06 22:25:35 +0000

    add dim_region, removed country column from sales_records

---
+++ models/dim_region.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT Region, Country 
+FROM `synthetic-nova-325421.TEST_DATASET.sales_records` 
+LIMIT 10
--- models/sales_records.sql
+++ models/sales_records.sql
@@ -1,4 +1,5 @@
 {{ config(materialized='table') }}
 
-SELECT * 
-FROM `synthetic-nova-325421.TEST_DATASET.sales_records` LIMIT 10
+SELECT  * except(Country)
+FROM `synthetic-nova-325421.TEST_DATASET.sales_records` 
+LIMIT 10
